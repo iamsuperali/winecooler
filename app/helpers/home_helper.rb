41a2343eb8faa40194cad13c8b9d528a13f89931
allen <iamsuperali@126.com>
2013-01-08 16:44:04 +0800

    Update app/helpers/home_helper.rb

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -4,7 +4,7 @@ module HomeHelper
     return_string = ""
     
     if p.thumb_id and Ckeditor::Asset.exists?(p.thumb_id)
-      return_string += "<a href='posts/#{p.id}' title='#{p.title}'>"
+      return_string += "<a href='/posts/#{p.id}' title='#{p.title}'>"
       return_string += "<img src='#{Ckeditor::Asset.find(p.thumb_id).url_thumb}' title='#{p.title}' alt='#{p.title}' class='colImgArt imgDetActu' />"
       return_string += "</a>"
     end
